0485cd05b3d7648a66ba749b20cf8c326719b8e2
zyl <431630364@qq.com>
2025-05-11 22:58:38 +0800

    [框架]-加载配置

--- user/main.go
+++ user/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"common/config"
+	"flag"
+)
+
+var configFile = flag.String("config", "application.yml", " config file")
+
+func main() {
+	//1.加载配置
+	flag.Parse()
+	config.InitConfig(*configFile)
+	//2.启动监控
+	//3.启动grpc服务
+
+}
